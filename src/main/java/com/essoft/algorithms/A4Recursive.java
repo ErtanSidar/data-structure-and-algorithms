dacaefbeac3b7fd37cfb1947c170f232b8ff89b8
ErtanSidar <essoft3@gmail.com>
2025-03-12 15:21:24 +0300

    a4 recursive sort

---
+++ src/main/java/com/essoft/algorithms/A4Recursive.java
@@ -0,0 +1,34 @@
+package com.essoft.algorithms;
+
+public class A4Recursive {
+
+    public static void main(String[] args) {
+        System.out.println(factorial(6));
+        System.out.println(fib(7));
+        System.out.println(pow(5, -2));
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else return n * factorial(n - 1);
+    }
+
+
+    public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        } else return fib(n - 1) + fib(n - 2);
+    }
+
+
+    public static double pow(double base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else if (exponent == 1) {
+            return base;
+        } else if (exponent < 0) {
+            return 1 / pow(base, -exponent);
+        } else return base * pow(base, exponent - 1);
+    }
+}
